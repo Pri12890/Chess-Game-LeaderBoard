e5aa94abef227f88580fd0d6226331d41f759ff5
Pri12890 <pri12890@gmail.com>
2018-03-27 21:27:20 -0500

    LearningGit

--- CreateProfile.java
+++ CreateProfile.java
@@ -5,7 +5,11 @@ public class CreateProfile {
 	String userName, password;
 	PlayerIdGenerator newID;
 	String description;
+
         String test;
+
+	String testGitPri; 
+//LearningGit
 	
 	public void FetchID()
 	{
